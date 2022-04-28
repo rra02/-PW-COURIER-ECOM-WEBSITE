fd3f81e3c9e4bf26160af2f7bfb7605aecb76407
rra02 <88827117+rra02@users.noreply.github.com>
2022-04-28 11:19:32 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../Fast-truck/css/about.css">
+</head>
+<body>
+<section class="background firstSection " style="background: url(bg.jpg);
+    background-size: cover;     height: auto;" >
+ <div class="about-section"  >
+      
+ <h1 style="
+    color: black;
+">About Us Page</h1>
+  
+<p style="
+    font-size: x-large;
+">"Fast-Truck" welcomes you to a safe and fastest delivery of your item.</p>
+  <p style="
+    font-size: x-large;
+">This is our about page and here you can get to see about our founder , co-founder and other's..</p>
+  <p style="
+    font-size: x-large;
+">If any doubt about services and order fell free to contact us by filling the contact form...</p>
+ </div>
+
+ <h2 style="text-align:center">Our Team</h2>
+ <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="/w3images/team1.jpg" alt="" style="width:100%">
+      <div class="container">
+        <h2>RESHAB</h2>
+        <p class="title">CEO & Founder</p>
+        <p>FOUNDED FAST-TRUCK IN 2018..</p>
+        <p>reshab@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+ </div>
+</section>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Fast-truck/css/style.css">
+    
+    <title>FAST-TRUCK E-COM SERVICE</title>
+   
+</head>
+<body>
+    
+    <nav class="navbar background">
+        <ul class="nav-list">
+            <div class="logo"><img src="lg.png" alt="logo"></div>
+            <li><a href="#home"> Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="#paras1">Services</a></li>
+            <li><a href="../Fast-truck/MULTI-USER-LOGIN/login.php">Login</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div class="rightNav">
+            <input type="text" name="Search" id="Search">
+            <button class="btn btn-sm">Search</button>
+        </div>
+    </nav>
+   
+       
+    
+    <section class="background firstSection" id="home">
+        <div class="box-main">
+            <div class="firstHalf">
+
+            
+
+            <p class="text-big">WELCOME TO FAST-TRUCK SERVICE</p>
+            <p class="text-small">"Fast-Truck" welcomes you to a safe and fastest delivery of your item.
+                We are here to phrovide you different types of courier services like "Same-day services",
+                "Overnight service" and many more.You can just sit at your home any by one click you parcel 
+                will be shipped to loaction you want to..
+            </p>
+            <div class="buttons">
+                <a href="../Fast-truck/MULTI-USER-LOGIN/register.php" button class="btn">Sign-up</button></a>
+            </div>
+
+
+
+            </div>
+            <div class="secondHalf">
+                <img src="lg.png" alt="image  ">
+            </div>
+        </div>
+    </section>
+    
+
+
+
+    <section class="secRight" id="paras1">
+        <div class="paras" >
+        <p class="sectionTag text-big">"OVERNIGHT SERVICE"</p>
+        <p class="sectionSubTag text-small">Want to get your parcel shipped the very next day? 
+            Then you must opt for overnight courier service.We ensure that it reaches the receiver in 24 hrs.</p>
+            
+        </div>
+        <div class="thumbnail">
+            <img src="os.jpg" alt="" class="imgFluid">
+
+        </div>
+    </section>
+    <hr>
+    <section class="secLeft left">
+        <div class="paras">
+        <p class="sectionTag text-big">"STANDARD COURIER-SERVICE"</p>
+        <p class="sectionSubTag text-small">If you are not in any urgency and want to opt for the cheapest mode 
+            of transferring your goods, then standard courier service is the best option..We ensure that it reaches the receiver within 6-7 working days.</p>
+        </div>
+        <div class="thumbnail">
+            <img src="standard.jpg" alt="" class="imgFluid">
+
+        </div>
+    </section>
+    <hr>
+    <section class="secRight">
+        <div class="paras">
+        <p class="sectionTag text-big">"INTERNATIONAL SERVICE"</p>
+        <p class="sectionSubTag text-small">This service varies greatly in price dependent on the size and weight
+            of what you are sending and how quickly you need it to get there. Usually took 3-4 days.</p>
+        </div>
+        <div class="thumbnail">
+            <img src="international.jpg" alt="" class="imgFluid">
+
+        </div>
+    </section>
+    <hr>
+    <section class="secLeft left">
+        <div class="paras">
+        <p class="sectionTag text-big">"SAME-DAY EXPRESS SERVICE"</p>
+        <p class="sectionSubTag text-small">It is mostly used by hospitals, medical suppliers, law and accounting firms.
+             It is for any urgent delivery within the city.
+
+        </p>
+        </div>
+        <div class="thumbnail">
+            <img src="sameday1.jpg" alt="" class="imgFluid">
+
+        </div>
+    </section>
+    
+    <hr>
+
+    <section class="contact background-img" id="contact">
+       <h2 class="text-form"> Contact Us</h2>
+       <div class="contactForm">
+        <form method="post">
+            
+           <input class= "contactForm-input" type="text" name="name" id="name" placeholder="Enter your name">
+           <input class= "contactForm-input"type="text" name="phone-number" id="phone-number" placeholder="Enter your phone-number">
+           <input class= "contactForm-input"type="text" name="email" id="email" placeholder="Enter your email">
+           
+        
+            <textarea class= "contactForm-input" name="text" id="text" cols="30" rows="5" placeholder="Elaborate your concern"></textarea>
+           
+           <input type="submit" value="Submit" name="submit" class="#" style="font-size: 18px;
+           background-color: #eb123e;
+           color:white;
+           border:0px;
+           padding:5px 20px;
+           border-radius: 15%;
+           cursor: pointer;">
+        </form>
+       </div>
+
+    </section>
+
+
+     <!--socialmedia  starts -->
+     <section class="socialmedia">
+        <div class="container text-centre">
+            <ul>
+                
+                <li>
+                    <a href="a"><img src="https://img.icons8.com/color/48/000000/facebook-circled--v4.png"/></a>
+                </li>
+                <li>
+                    <a href="a"><img src="https://img.icons8.com/color/48/000000/instagram-new--v2.png"/></a>
+                </li>
+                <li>
+                    <a href="a"><img src="https://img.icons8.com/color/48/000000/twitter-circled--v3.png"/></a>
+                </li>
+
+            </ul>
+        </div>
+    </section>
+
+  
+    <!--socialmedia sec ends -->
+ <!--footer  starts -->
+ 
+ <footer class="background">
+
+    <p class="text-footer">
+        Copyright &copy; 2025  www.FAST-TRUCK.com - All rights reserved
+    </p>
+
+ </footer>
+<!--footer sec ends -->
+</body>
+</html>
